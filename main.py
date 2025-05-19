5cbe491c71d70bcdf894021df681e121b00fc337
Ensar Akbaş <ensarr.akbas@gmail.com>
2025-05-19 23:55:00 +0300

    Senaryo dosyaları ve gerekli python dosyaları oluşturuldu

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from data_generator import generate_random_drones, generate_random_deliveries, generate_random_nofly_zones
 
 
-drones = generate_random_drones(1)
-deliveries = generate_random_deliveries(1)
-nofly_zones = generate_random_nofly_zones(1)
+drones = generate_random_drones(10)
+deliveries = generate_random_deliveries(50)
+nofly_zones = generate_random_nofly_zones(5)
 
 for drone in drones:
     print(vars(drone))
